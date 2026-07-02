625611a29f60f98649b8fac6c93db7d5066d7f27
Nareiki <christianredl98@gmx.at>
2026-07-02 18:35:55 +0200

    Deleted: Removed unnecessary files that are not used anymore

--- Backend/TourPlanner.Application/Common/ApiResultOfT.cs
+++
@@ -1,9 +0,0 @@
-namespace TourPlanner.Application.Common;
-
-public sealed record ApiResult<T>(bool Success, T? Data = default, string? Message = null)
-{
-    public static ApiResult<T> Ok(T data, string? message = null) => new(true, data, message);
-
-    public static ApiResult<T> Fail(string message) => new(false, default, message);
-}
-
--- Backend/TourPlanner.Application/Common/SearchResponse.cs
+++
@@ -1,4 +0,0 @@
-namespace TourPlanner.Contracts.Common;
-
-public sealed record SearchResponse<TTourSummary, TTourLog>(IReadOnlyList<TTourSummary> Tours, IReadOnlyList<TTourLog> TourLogs);
-
