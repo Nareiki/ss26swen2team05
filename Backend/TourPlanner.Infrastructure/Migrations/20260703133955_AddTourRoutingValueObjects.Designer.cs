51df9c10d833644fa65e1d0595cbe42c9d02feb0
Nareiki <christianredl98@gmx.at>
2026-07-03 15:49:33 +0200

    Added: Migration for the new database values

--- Backend/TourPlanner.Infrastructure/Migrations/20260703133955_AddTourRoutingValueObjects.Designer.cs
+++ Backend/TourPlanner.Infrastructure/Migrations/20260703133955_AddTourRoutingValueObjects.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TourPlanner.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using TourPlanner.Infrastructure.Persistence;
 namespace TourPlanner.Infrastructure.Migrations
 {
     [DbContext(typeof(TourPlannerDbContext))]
-    partial class TourPlannerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260703133955_AddTourRoutingValueObjects")]
+    partial class AddTourRoutingValueObjects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
